fa7d1cd580d388deec77a7c6531e7502665eafac
AtomMushroom <35073893+AtomMushroom@users.noreply.github.com>
2022-02-10 19:42:46 +0300

    Add files via upload

---
+++ Lesson_4Task4/Lesson_4Task4/L4Task4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson_4Task3
+{
+    class L4Task4
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double numb;
+            int counter = 1;
+            while (counter <= 5)
+            {
+                numb = Double.Parse(Console.ReadLine());
+                sum += numb;
+                counter++;
+            }
+            Console.WriteLine("Сумма введенных чисел: " + sum);
+        }
+    }
+}
---
+++ Lesson_4Task5/Lesson_4Task5/L4Task5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson_4Task5
+{
+    class L4Task5
+    {
+        static void Main(string[] args)
+        {
+            int numb;
+            int counter = 0;
+            int max = int.MinValue;
+            Console.WriteLine("Введите последовательность чисел и закончите ввод нулем");
+            do
+            {
+                numb = Int32.Parse(Console.ReadLine());
+                if (numb!=0 && numb > max)
+                {
+                    max = numb;
+                    counter++;
+                }
+            } while (numb != 0);
+            Console.WriteLine("Максимальное число: " + max);
+            Console.WriteLine("Его порядковый номер: " + counter);
+        }
+    }
+}
